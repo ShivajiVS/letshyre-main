8cd581b393f3823c49eb277ba103baa8eb5327d4
kondeti shivaji <shivajikondeti555@gmail.com>
2026-06-10 11:47:20 +0530

    social links are updated in footer section

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -34,15 +34,24 @@ export function Footer() {
           </p>
 
           <div className="ft-socials">
-            <button className="ft-social-icon">
+            <a href="https://wa.me/919346708639" target="_blank" rel="noopener noreferrer" className="ft-social-icon">
               <i className="bi bi-whatsapp"></i>
-            </button>
-            <button className="ft-social-icon">
+            </a>
+            <a href="https://x.com/letshyreAi" target="_blank" rel="noopener noreferrer" className="ft-social-icon">
+              <i className="bi bi-twitter-x"></i>
+            </a>
+            <a href="https://www.linkedin.com/company/letshyre/" target="_blank" rel="noopener noreferrer" className="ft-social-icon">
+              <i className="bi bi-linkedin"></i>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=61585708555581" target="_blank" rel="noopener noreferrer" className="ft-social-icon">
               <i className="bi bi-facebook"></i>
-            </button>
-            <button className="ft-social-icon">
+            </a>
+            <a href="https://www.instagram.com/letshyreofficial/" target="_blank" rel="noopener noreferrer" className="ft-social-icon">
               <i className="bi bi-instagram"></i>
-            </button>
+            </a>
+            <a href="https://www.youtube.com/@LetsHyre" target="_blank" rel="noopener noreferrer" className="ft-social-icon">
+              <i className="bi bi-youtube"></i>
+            </a>
           </div>
         </div>
 
